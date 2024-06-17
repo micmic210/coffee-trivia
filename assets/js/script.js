55f14e51c8093042156b7cf1655d765b6f6b127a
Michiko  Inoue <inouem888@gmail.com>
2024-06-17 08:41:25 +0000

    Add initial quiz data.

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Add quiz data
+
+const quizData = [
+    {
+        question: "Which country is espresso originally from?",
+        options: ["Italy", "Brazil", "France", "USA"],
+        correct: "Italy"
+    },
+    {
+        question: "What is used in a cappuccino?",
+        options: ["Milk", "Cream", "Water", "Soda"],
+        correct: "Milk"
+    },
+    {
+        question: "Which of the following is not a type of coffee?",
+        options: ["Americano", "Latte", "Mocha", "Tea"],
+        correct: "Tea"
+    }
+  ];
+ let currentQuestion = 0;
+ let score = 0;
